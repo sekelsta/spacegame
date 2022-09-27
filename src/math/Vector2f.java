ba699398c51d7ad0e8da5f541753ea8f11118c14
sekelsta <sekelsta@protonmail.com>
2022-09-27 13:18:27 -0400

    Update file headers

--- src/math/Vector2f.java
+++ src/math/Vector2f.java
@@ -1,5 +1,17 @@
 package sekelsta.math;
 
+// This Vector2f class is released under a CC0 licence.
+// Feel free to use it in your own projects with or without attribution. There
+// is only so much creativity that can go into a vector class, anyway.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 public class Vector2f {
     public float x, y;
 
