e2fe8db240cb5c7dded60ce5868f66466ad36a8e
sekelsta <sekelsta@protonmail.com>
2022-11-11 12:44:17 -0500

    Make the projectile look fancier

--- src/game/render/entity/ProjectileRenderer.java
+++ src/game/render/entity/ProjectileRenderer.java
@@ -1,8 +1,11 @@
 package sekelsta.game.render.entity;
 
+import java.awt.Color;
 import java.util.Scanner;
 
+import sekelsta.engine.render.MatrixStack;
 import sekelsta.engine.render.Texture;
+import sekelsta.engine.render.Textures;
 import sekelsta.engine.render.entity.EntityRenderer;
 import sekelsta.engine.render.mesh.RigidMesh;
 
@@ -11,8 +14,25 @@ import sekelsta.game.entity.Projectile;
 import sekelsta.tools.ObjParser;
 
 public final class ProjectileRenderer extends EntityRenderer<Projectile> {
+    protected RigidMesh border;
+    protected RigidMesh center;
+    protected Texture glow;
+
     public ProjectileRenderer() {
-        mesh = new RigidMesh(ObjParser.parse(new Scanner(EntityRenderer.class.getResourceAsStream("/assets/obj/projectile.obj"))));
-        texture = new Texture("oily.jpg");
+        center = new RigidMesh(ObjParser.parse(new Scanner(EntityRenderer.class.getResourceAsStream("/assets/obj/projectile.obj"))));
+        border = new RigidMesh(ObjParser.parse(new Scanner(EntityRenderer.class.getResourceAsStream("/assets/obj/projectile_border.obj"))));
+        glow = new Texture(new Color(0x6c, 0xe0, 0xff));
+        texture = Textures.BLACK;
+        specular = Textures.BLACK;
+    }
+
+    @Override
+    public void render(Projectile entity, float lerp, MatrixStack stack) {
+        mesh = border;
+        emission = glow;
+        super.render(entity, lerp, stack);
+        mesh = center;
+        emission = Textures.BLACK;
+        super.render(entity, lerp, stack);
     }
 }
